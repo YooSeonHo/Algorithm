2c8693b83f0fd4d5dcde8afa64e18338f722fca7
Yoo SeonHo <tjsgh503@naver.com>
2024-01-03 14:18:14 +0900

    DP 회의실 등

---
+++ DP/19622 회의실 배정3.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+meeting = [list(map(int,input().split())) for _ in range(n)]
+dp = [0] * 100001
+dp[0] = meeting[0][2]
+dp[1] = meeting[1][2]
+dp[2] = dp[0] + meeting[2][2]
+
+for i in range(3,n) :
+    dp[i] = max(dp[i-2],dp[i-3]) + meeting[i][2]
+
+print(max(dp))
--- DP/[다시]1932 정수 삼각형.py
+++ DP/[다시]1932 정수 삼각형.py
@@ -1,3 +1,43 @@
+
+
+
+n = int(input())
+triangle = [ list(map(int,input().split())) for _ in range(n)]
+dp = [[0] * (n) for _ in range(n)]
+dp[0][0] = triangle[0][0]
+
+for i in range(1,n) :
+    for j in range(i+1) :
+        if j == 0 :
+            dp[i][j] = dp[i-1][j] + triangle[i][j]
+            continue
+        
+        if j == i+1 :
+            dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            continue
+
+        dp[i][j] = max(dp[i-1][j],dp[i-1][j-1]) + triangle[i][j]
+
+print(max(dp[-1]))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # import sys
 # input = sys.stdin.readline
 
