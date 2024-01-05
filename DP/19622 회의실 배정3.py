c0e243849ea8ec2c36d7aca65553a91786dad637
Yoo SeonHo <tjsgh503@naver.com>
2024-01-05 14:50:59 +0900

    DP

--- DP/19622 회의실 배정3.py
+++ DP/19622 회의실 배정3.py
@@ -4,10 +4,12 @@ input = sys.stdin.readline
 n = int(input())
 
 meeting = [list(map(int,input().split())) for _ in range(n)]
-dp = [0] * 100001
+dp = [0] * n
 dp[0] = meeting[0][2]
-dp[1] = meeting[1][2]
-dp[2] = dp[0] + meeting[2][2]
+if n >= 2 :
+    dp[1] = meeting[1][2]
+    if n >= 3 :
+        dp[2] = dp[0] + meeting[2][2]
 
 for i in range(3,n) :
     dp[i] = max(dp[i-2],dp[i-3]) + meeting[i][2]
